84d02448a0a40005ddb4fddeebbc10bf6fd4d7b2
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2017-11-12 20:12:51 +0530

    Add an API endpoint for fetching posts

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const url = 'https://medium.com/@' + (process.env.SLUG || 'dheerajhere') +'/latest?format=json';
+const allowedOrigin = process.env.ORIGIN || '*';
+
+app.get('/', (req, res) => {
+	res.send('Hi there! This is a proxy service that allows me to display my medium.com posts on the website.');
+});
+
+app.get('/posts', (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', allowedOrigin);
+	res.header('Access-Control-Allow-Methods', 'GET');
+
+	// handle preflight requests
+	if (req.method === 'OPTIONS') {
+		res.send();
+	} else {
+		request({url: url, method: 'GET'}, (err, res) => {
+			if (err) {
+				console.log('Error: ' + res.statusCode);
+			}
+		}).pipe(res);
+	}
+});
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => console.log('Application started on port ' + app.get('port')));
